524c369b20d465744a66e766875e60493448faf2
ameeenamzz <shoukethameen@gmail.com>
2025-07-13 18:42:07 +0530

    Implement task input handling and update AddTask component structure

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddTask = () => {
+  const [taskInput, setTaskInput] = useState("");
+  const [taskArray, setTaskArray] = useState([]);
+  //   console.log(taskInput)
+  const handleAddTask = () => {
+    setTaskInput(taskArray.push(taskInput));
+    setTaskInput("")
+    console.log(taskArray);
+  };
+
   return (
     <div className="flex justify-center mt-40">
       <div>
-        <input type="text" placeholder="Type here" className="input w-96" />
-        <div className="">
-          <button className="btn btn-neutral">Neutral</button>
-          <button className="btn btn-primary">Primary</button>
+        <input
+          value={taskInput}
+          type="text"
+          placeholder="Type here"
+          className="input w-96"
+          onChange={(e) => setTaskInput(e.target.value)}
+        />
+        <div className="mt-5">
+          <button className=" btn btn-neutral w-full" onClick={handleAddTask}>
+            Add Task
+          </button>
+          <button className="btn btn-primary w-full">
+            Ask Ai to Create Task
+          </button>
         </div>
       </div>
     </div>
