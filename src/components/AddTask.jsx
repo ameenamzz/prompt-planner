2744a948998a55789d628171d2b81dc755766dad
ameeenamzz <shoukethameen@gmail.com>
2025-07-14 12:17:48 +0530

    Add context provider for task management and implement task removal functionality

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,47 +1,51 @@
-import React, { useState } from "react";
+import React, { createContext, useState } from "react";
 import ListTask from "./ListTask";
-
+export const ListContext = createContext();
 const AddTask = () => {
   const [taskInput, setTaskInput] = useState("");
   const [taskArray, setTaskArray] = useState([]);
-  //   console.log(taskInput)
+
   const handleAddTask = () => {
     if (taskInput === "") return null;
     setTaskInput(taskArray.push(taskInput));
     setTaskInput("");
-    console.log(taskArray);
   };
 
   return (
-    <>
-      <div className="flex justify-center mt-40">
-        <div>
-          <input
-            value={taskInput}
-            type="text"
-            placeholder="Type here"
-            className="input w-96"
-            onChange={(e) => setTaskInput(e.target.value)}
-          />
-          <div className="mt-5">
-            <button className=" btn btn-neutral w-full" onClick={handleAddTask}>
-              Add Task
-            </button>
-            <button className="btn btn-primary w-full">
-              Ask Ai to Create Task
-            </button>
+    <ListContext.Provider value={setTaskArray}>
+      <>
+        <div className="flex justify-center mt-40">
+          <div>
+            <input
+              value={taskInput}
+              type="text"
+              placeholder="Type here"
+              className="input w-96"
+              onChange={(e) => setTaskInput(e.target.value)}
+            />
+            <div className="mt-5">
+              <button
+                className=" btn btn-neutral w-full"
+                onClick={handleAddTask}
+              >
+                Add Task
+              </button>
+              <button className="btn btn-primary w-full">
+                Ask Ai to Create Task
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-      <div className=" mt-20">
-        {taskArray.length === 0 ? null : (
-          <h1 className="flex justify-center">Tasks To Be Completed</h1>
-        )}
-        {taskArray.map((list) => (
-          <ListTask data={list} />
-        ))}
-      </div>
-    </>
+        <div className=" mt-20">
+          {taskArray.length === 0 ? null : (
+            <h1 className="flex justify-center">Tasks To Be Completed</h1>
+          )}
+          {taskArray.map((list) => (
+            <ListTask data={list} />
+          ))}
+        </div>
+      </>
+    </ListContext.Provider>
   );
 };
 
--- src/components/ListTask.jsx
+++ src/components/ListTask.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ListContext } from "./AddTask";
 
 const ListTask = ({ data }) => {
+  const setTaskArray = useContext(ListContext);
+  const handleRemoveTask = (taskToRemove) => {
+    setTaskArray((prevTasks) => prevTasks.filter((t) => t !== taskToRemove));
+  };
   return (
     <div className="flex justify-center mt-2 ">
       <div className="card card-dash bg-base-300 w-96">
         <div className="card-body flex flex-row ">
-          <input
-            type="checkbox"
-            className="checkbox checkbox-success"
-          />
-          <p className="w-40 break-words">{data}</p>
+          <input type="checkbox" className="checkbox checkbox-success" />
+          <p className="w-40 break-words px-2">{data}</p>
+          <button
+            className="btn btn-error btn-sm"
+            onClick={() => handleRemoveTask(data)}
+          >
+            x
+          </button>
         </div>
       </div>
     </div>
