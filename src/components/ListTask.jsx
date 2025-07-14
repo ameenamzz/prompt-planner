c363d22a88845fc0c4017e0d69a76fbb484ecd6b
ameeenamzz <shoukethameen@gmail.com>
2025-07-14 08:10:04 +0530

    Refactor ListTask component to include checkbox for task completion

--- src/components/ListTask.jsx
+++ src/components/ListTask.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 const ListTask = ({ data }) => {
   return (
-    <div className="flex justify-center mt-2">
+    <div className="flex justify-center mt-2 ">
       <div className="card card-dash bg-base-300 w-96">
-        <div className="card-body">
-          <p>{data}</p>
+        <div className="card-body flex flex-row ">
+          <input
+            type="checkbox"
+            className="checkbox checkbox-success"
+          />
+          <p className="w-40 break-words">{data}</p>
         </div>
       </div>
     </div>
